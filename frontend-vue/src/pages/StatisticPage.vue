6912514df01665d7ac24d02c94619b74f5f0de14
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2025-06-07 16:27:36 +0700

    feat: add statistic graph

---
+++ frontend-vue/src/pages/StatisticPage.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts">
+import { ref, watchEffect, type Ref } from 'vue';
+import { VisXYContainer, VisLine, VisAxis, VisCrosshair, VisTooltip, VisScatter, VisBulletLegend } from '@unovis/vue';
+
+import Footer from '../components/Footer.vue';
+import { generateTimestamp } from '../utils';
+import type { TroubleDataRecordByDay, TroubleDataRecordByFactory } from '../types';
+import StatisticChart from '../composables/stats';
+
+const statistic = new StatisticChart();
+const datesByDay = ref<Date[] | undefined>([new Date(), new Date()]);
+const datesByFactory = ref<Date[] | undefined>([new Date(), new Date()]);
+
+const troubleRecordByDay = ref<TroubleDataRecordByDay[]>([]);
+const troubleRecordByFactory = ref<TroubleDataRecordByFactory[]>([]);
+
+const xByDay = (d: TroubleDataRecordByDay, i: number): number => i;
+const yByDay = [(d: TroubleDataRecordByDay): number => d.value];
+const xByFactory = (d: TroubleDataRecordByFactory, i: number): number => i;
+const yByFactory = [
+  (d: TroubleDataRecordByFactory): number => d.f1,
+  (d: TroubleDataRecordByFactory): number => d.f2,
+  (d: TroubleDataRecordByFactory): number => d.f3,
+  (d: TroubleDataRecordByFactory): number => d.f4,
+  (d: TroubleDataRecordByFactory): number => d.subpro,
+];
+
+const datePickerRangeFormat = (dates: Ref<Date[] | undefined>): Ref<string[]> => {
+  if (!dates.value) return ref<string[]>([]);
+  if (!dates.value[1]) return ref<string[]>([]);
+  const startDate = dates.value[0];
+  const endDate = dates.value[1];
+
+  let current = new Date(startDate);
+  let dateRange: Ref<string[]> = ref<[]>([]);
+
+  for (let i = startDate.getDate(); i <= endDate.getDate(); i++) {
+    dateRange.value.push(current.toISOString());
+    current.setDate(current.getDate() + 1);
+  }
+  return dateRange;
+};
+
+const xTicksLabelByDay = (i: number): string => {
+  const dateRange = datePickerRangeFormat(datesByDay);
+  if (dateRange.value.length == 0) return `<p class="text-[0.1rem]">null</p>`;
+  return `${generateTimestamp(dateRange.value[i])[1]}`;
+};
+const xTicksLabelByFactory = (i: number): string => {
+  const dateRange = datePickerRangeFormat(datesByFactory);
+  if (dateRange.value.length == 0) return `<p class="text-[0.1rem]">null</p>`;
+  return `${generateTimestamp(dateRange.value[i])[1]}`;
+};
+
+const xTickLabelLengthByDay = (): number => {
+  return troubleRecordByDay.value.length >= 15 ? 15 : troubleRecordByDay.value.length - 1;
+};
+const xTickLabelLengthByFactory = (): number => {
+  return troubleRecordByFactory.value.length >= 15 ? 15 : troubleRecordByFactory.value.length - 1;
+};
+
+const templateByDay = (d: TroubleDataRecordByDay) => `<p class="text-xs">${d.value} trouble</p>`;
+const templateByFactory = (d: TroubleDataRecordByFactory) => `<p class="text-xs">F1 : ${d.f1}<br/> F2 : ${d.f2}<br/> F3 : ${d.f3}<br/> F4 : ${d.f4}<br/> Subpro : ${d.subpro}</p>`;
+
+const items = [
+  {
+    name: 'Factory 1',
+  },
+  {
+    name: 'Factory 2',
+  },
+  {
+    name: 'Factory 3',
+  },
+  {
+    name: 'Factory 4',
+  },
+  {
+    name: 'Subproduction',
+  },
+];
+
+watchEffect(async () => {
+  if (!datesByDay.value) return;
+  if (datesByDay.value.length != 2) return;
+
+  if (!datesByFactory.value) return;
+  if (datesByFactory.value.length != 2) return;
+
+  await statistic.totalTroubleInDay(troubleRecordByDay, datePickerRangeFormat(datesByDay));
+  await statistic.totalTroubleByFactory(troubleRecordByFactory, datePickerRangeFormat(datesByFactory));
+});
+</script>
+<template>
+  <div>
+    <main class="min-h-screen">
+      <header class="flex items-center justify-between bg-white p-5">
+        <div class="flex items-center gap-5">
+          <button @click="$router.back()">
+            <i class="pi pi-arrow-left"></i>
+          </button>
+          <h1>Statistic</h1>
+        </div>
+      </header>
+      <Card class="mt-2">
+        <template #content>
+          <section class="flex flex-col justify-center items-start gap-5">
+            <div class="flex items-center justify-between w-full">
+              <span class="poppins-semibold text-xs">Total Trouble in a day</span>
+              <DatePicker v-model="datesByDay" selectionMode="range" size="small" showIcon fluid :showOnFocus="false" placeholder="Select dates" :inputStyle="{ fontSize: '0.6rem', width: '9.5rem' }" />
+            </div>
+            <VisXYContainer :data="troubleRecordByDay" :margin="{ right: 15 }" :height="200" :key="datesByDay">
+              <VisLine :x="xByDay" :y="yByDay" :lineWidth="2" />
+              <VisAxis type="x" :x="xByDay" :tickFormat="xTicksLabelByDay" :tickTextAngle="10" :gridLine="false" label="Day" :labelMargin="5" :tickLine="false" :numTicks="xTickLabelLengthByDay()" />
+              <VisAxis type="y" :y="yByDay" :gridLine="false" label="Total" :labelMargin="5" :tickLine="false" />
+              <VisCrosshair :template="templateByDay" color="rgb(77, 140, 253)" />
+              <VisScatter :x="xByDay" :y="yByDay" :size="5" />
+              <VisTooltip />
+            </VisXYContainer>
+          </section>
+        </template>
+      </Card>
+      <Card class="mt-2">
+        <template #content>
+          <section class="flex flex-col justify-center items-start gap-5">
+            <div class="flex items-center justify-between w-full">
+              <span class="poppins-semibold text-xs">Total Trouble by Factory</span>
+              <DatePicker v-model="datesByFactory" selectionMode="range" size="small" showIcon fluid :showOnFocus="false" placeholder="Select dates" :inputStyle="{ fontSize: '0.6rem', width: '9.5rem' }" />
+            </div>
+            <VisXYContainer :data="troubleRecordByFactory" :margin="{ right: 15 }" :height="180" :key="datesByFactory">
+              <VisLine :x="xByFactory" :y="yByFactory" :lineWidth="2" />
+              <VisAxis type="x" :x="xByFactory" :tickFormat="xTicksLabelByFactory" :tickTextAngle="10" :gridLine="false" label="Day" :labelMargin="5" :tickLine="false" :numTicks="xTickLabelLengthByFactory()" />
+              <VisAxis type="y" :y="yByFactory" :gridLine="false" label="Total" :labelMargin="5" :tickLine="false" />
+              <VisCrosshair :template="templateByFactory" color="rgb(77, 140, 253)" />
+              <VisScatter :x="xByFactory" :y="yByFactory" :size="5" />
+              <VisTooltip />
+            </VisXYContainer>
+            <VisBulletLegend :items="items" class="text-[0.5rem]"/>
+          </section>
+        </template>
+      </Card>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
